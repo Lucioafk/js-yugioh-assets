6238af2493aee62a5c9e67c47361333f13a7b0e7
Lucioafk <luciogabriel1550@gmail.com>
2026-01-09 19:50:59 -0300

    negocios ai

---
+++ src/scripts/engine.js
@@ -0,0 +1,67 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel")
+    },
+};
+
+const playerSide = { 
+    player1: "player-field-card",
+    computer: "computer-field-card"
+}
+
+const pathImages = "src/assets/icons/";
+
+const cardData = [
+    {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}dragon.png`,
+    WinOf: [1],
+    LoserOf: [2],
+    },
+    {
+    id: 1,
+    name: "Dark Magician",
+    type: "Pedra",
+    img: `${pathImages}magician.png`,
+    WinOf: [2],
+    LoserOf: [0],
+    },
+    {
+    id: 2,
+    name: "Exodia",
+    type: "Scissor",
+    img: `${pathImages}exodia.png`,
+    WinOf: [0],
+    LoserOf: [1],
+    },
+];
+
+async function drawCards(cardNumbers, fieldSide) {
+    for(let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId ();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage)
+    }
+}
+
+function init(){
+    drawCards(5, playerSide.player1);
+    drawCards(5,playerSides.computer);
+};
